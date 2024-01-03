81eb41000c5c88be51241c7c8f65eb4fc8f47bd4
FTMustafa <mustafamustafatw@gmail.com>
2024-01-03 04:17:01 +0300

    .

--- lib/pages/araba.dart
+++ lib/pages/araba.dart
@@ -74,7 +74,7 @@ class ArabaState extends State<Araba> {
     );
   }
 
-  bool durum1 = true; //marka seçildi
+  bool durum1 = false; //marka seçildi
   bool durum2 = false; //model seçildi
   bool durum3 = false; //yıl seçildi
 
@@ -196,7 +196,7 @@ class ArabaState extends State<Araba> {
                                   setState(() {
                                     selectedMarka = value;
                                     selectedModel = null;
-                                    durum2 = false;
+                                    durum1 = true;
                                   });
                                 },
                               )

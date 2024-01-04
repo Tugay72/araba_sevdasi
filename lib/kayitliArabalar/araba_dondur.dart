8fd25ee4a510f3c652d96c9b5a7809e6bcc5c3cf
Naz Hacıhafızoğlu <nazhacihafizoglu@gmail.com>
2024-01-04 04:50:27 +0300

    kayıtlı arabalara araba eklenebiliyor ve silinebiliyor.

--- lib/kayitliArabalar/araba_dondur.dart
+++ lib/kayitliArabalar/araba_dondur.dart
@@ -1,41 +1,55 @@
-import 'package:araba_sevdasi/kayitliArabalar/araba_liste.dart';
 import 'package:flutter/material.dart';
+import 'package:araba_sevdasi/pages/araba.dart';
 
-class arabaDondur extends StatelessWidget {
+class arabaDondur extends StatefulWidget {
   arabaDondur();
+
+  @override
+  _arabaDondurState createState() => _arabaDondurState();
+}
+
+class _arabaDondurState extends State<arabaDondur> {
+  void arabaSil(int index) {
+    setState(() {
+      kayitliArabaListesi.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var liste = ArabaListe.arabaliste;
     return SizedBox(
       child: ListView.builder(
-        itemCount: liste.length,
-        itemBuilder: (context, i) => ListTile(
-          title: Text(
-            "${liste[i].marka} ${liste[i].model}",
-            style: const TextStyle(
-              fontSize: 25,
-              fontFamily: "Oswald",
-              fontWeight: FontWeight.w400,
+        itemCount: kayitliArabaListesi.length,
+        itemBuilder: (context, index) {
+          var araba = kayitliArabaListesi[index];
+          return ListTile(
+            title: Text(
+              "${araba['marka']} ${araba['model']}",
+              style: const TextStyle(
+                fontSize: 25,
+                fontFamily: "Oswald",
+                fontWeight: FontWeight.w400,
+              ),
             ),
-          ),
-          subtitle: Text(
-            liste[i].yil.toString(),
-            style: const TextStyle(
-              fontSize: 20,
-              fontFamily: "Oswald",
-              fontWeight: FontWeight.w400,
+            subtitle: Text(
+              araba['uretimYil'].toString(),
+              style: const TextStyle(
+                fontSize: 20,
+                fontFamily: "Oswald",
+                fontWeight: FontWeight.w400,
+              ),
             ),
-          ),
-          trailing: TextButton(
-            child: const Icon(Icons.remove_circle_outline_rounded),
-            onPressed: () {
-              //silme işlevi eklenecek
+            trailing: TextButton(
+              child: const Icon(Icons.remove_circle_outline_rounded),
+              onPressed: () {
+                arabaSil(index);
+              },
+            ),
+            onTap: () {
+              // seçili arabanın özelliklerini ve istatistiklerini görüntüleyen sayfa eklenecek
             },
-          ),
-          onTap: () {
-            //seçili arabanın özelliklerini ve istatistiklerini görüntüleyen sayfa eklenecek
-          },
-        ),
+          );
+        },
       ),
     );
   }
--- lib/kayitliArabalar/araba_liste.dart
+++
@@ -1,8 +0,0 @@
-import 'package:araba_sevdasi/kayitliArabalar/araba_class.dart';
-
-class ArabaListe {
-  static List<ArabaClass> arabaliste = [
-    ArabaClass("Audi", "A8", 2012),
-    ArabaClass("Ford", "Focus", 2001),
-  ];
-}
